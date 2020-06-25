e1bce3539b5219f0592e64255cc9ea7903e3d68c
Pham <ngoc.pham.nm@gmail.com>
2020-06-24 22:07:33 -0500

    Update assignment 1
    optimize SIN number checking

--- Assignments/NgocPhamA1Q1.py
+++ Assignments/NgocPhamA1Q1.py
@@ -8,52 +8,130 @@ Created on Thu Jun 18 13:49:24 2020
 #Question 1: Required
 print("***Question 1: Required***")
 
-
+#----------------------------------------------
 #Step 1 - Reading file:
-headings = []
-observations = []
-data = []
+headings = [] #["maxTemp","minTemp"]
+observations = []  #[[all values in maxTemp column],[all values in minTemp column]]
+data = [] #data = [maxTemp's val,minTemp's val]
 
 lineNum=0
 
 reducedweather = open("reducedweather1.csv", 'r')
 headings = reducedweather.readline().strip().split(',')
-colNum = len(headings)
+columnNum = len(headings)
 
 
-for col in range(colNum):
+for eachInteration in range(columnNum):
     observations.append([])  #create two empty lists nested inside a list "observations"
 
-for line in reducedweather:
-    data = line.split(',')   #data is [maxtemp,mintemp]
+for eachLine in reducedweather:
+    data = eachLine.split(',')   #data = [maxTemp's val, minTemp's val]
     lineNum += 1
     
-    for listIndex in range(colNum):
-        observations[listIndex].append(float(data[listIndex]))
+    for eachList in range(columnNum):
+        observations[eachList].append(float(data[eachList])) #list1 contains all maxtemp values, list2 contains all mintemp values in observations
 
 
+#----------------------------------------------
 #Step 2 - Calculating mean, highest and lowest temperature:
-for listIndex in colNum:
-    mean = sum(observations[listIndex])/len(observations[listIndex])
+means = [] #[[maxTemp's mean],[minTemp's mean]]
+maxVals = [] #maxTemp's maximum value, minTemp's maximum value
+minVals = [] #maxTemp's minimum value, minTemp's minimum value
 
-print(observations)
+for eachList in range(columnNum):
+    sumList = sum(observations[eachList])
+    meanList = sumList/len(observations[eachList])
+    means.append(meanList)
+    maxList = max(observations[eachList])
+    maxVals.append(maxList)
+    minList = min(observations[eachList])
+    minVals.append(minList)
 
+
+
+# print(observations)
+# print(lineNum)
+# print(len(observations[0]))
+# print(len(observations[1]))
+# print(means)
+# print(maxVals)
+# print(minVals)
+
+
+#----------------------------------------------
 #Step 3 - Calculating Standard Deviation    
+import math
+squaredDif = [] # squaredDif = [[maxTemp's list of squared differences],[minTemp's list of squared differences]]
+sumSqDif = [] # sumSqDif = [maxTemp's sum of squared dif, minTemp's sum of squared dif]
+stdDevs = [] # stdDev = [maxTemp's stddev, minTemp's stddev]
+
+for eachInteration in range(columnNum):
+    squaredDif.append([])  #create two empty lists nested inside a list "squaredDif"
+
+for eachList in range(columnNum):
+    sumSquaredDif = 0
+    for eachVal in range(len(observations[eachList])):
+        squaredDifVal = math.pow(observations[eachList][eachVal]-means[eachList],2)
+        squaredDif[eachList].append(squaredDifVal)
+        sumSquaredDif += squaredDifVal
+    sumSqDif.append(sumSquaredDif)    
+    stdDevVal = math.sqrt(sumSquaredDif/len(observations[eachList]))
+    stdDevs.append(stdDevVal)
 
 
-
+#----------------------------------------------
 #Step 4 - Printing Output:
 
+line0 =        '{0:^14s}|{1:^15s}|{2:^15s}|{3:^15s}|{4:^15s}'    # ^ default center text, s for formatting as string
+iteratedLine0 ='{0:>14s}|{1:>15.2f}|{2:>15.2f}|{3:>15.2f}|{4:>15.2f}' # > default right align
+iteratedLine1 = '-'* 14 + '+' + '-'* 15 + '+' + '-'* 15 + '+' + '-'* 15 + '+' + '-'* 15
 
+print()
+print(line0.format("Column Names","Means","Std Deviation","Highest Score","Lowest Score"))
+print(iteratedLine1)
+for eachList in range(columnNum):
+    print(iteratedLine0.format(headings[eachList],means[eachList],stdDevs[eachList],maxVals[eachList],minVals[eachList]))
+    print(iteratedLine1)
+
+print()
 reducedweather.close()
+
+
+
+
+#----------------------------------------------
 #Question 2: Optional
 print("***Question 2: Optional***")
 
+SIN = input("Please enter a SIN number: > ")
+number = '121212121'
+stringTotal = ''
+total = 0
+print()
+
+if len(SIN)!=9 or SIN.isdigit() == False:  #check input is 9-digit and input is a number, no chars allowed
+    print("You have entered an invalid SIN number. Please check SIN again.")
+else:
+    for eachDigit in range(len(SIN)):
+        digit = int(SIN[eachDigit])
+        multiplier = int(number[eachDigit])
+        product = digit * multiplier
+        stringTotal += str(product)
+        
+    for eachStringDigit in range(len(stringTotal)):
+        value = int(stringTotal[eachStringDigit])
+        total += value
+        
+    if total % 10 == 0:
+        print("Congrats! This is a valid SIN number.")
+    else:
+        print("Unfortunately, this is NOT a valid SIN number.")
 
 
 
-
+#----------------------------------------------
 #Programmed by...
+print()
 import time
 print ("\nProgrammed by Ngoc Pham (Student number: 7891063)")
 print ("Date: " + time.ctime())
--- Labs/lab3.py
+++ Labs/lab3.py
@@ -6,96 +6,75 @@ Created on Mon Jun 22 16:45:45 2020
 """
 
 
-# #Exercise 1: Sum of values in a file
-# print("---Exercise 1: Sum of values in a file---")
-# print()
-# count = 0
-# total = 0
-# infile = open("lab3text.txt",'r')
+#Exercise 1: Sum of values in a file
+print("---Exercise 1: Sum of values in a file---")
+print()
+count = 0
+total = 0
+infile = open("lab3text.txt",'r')
 
-# for eachline in infile:
-#     count += 1
-#     total += int(eachline)
-
-
-# print("Numbers of numbers: {}".format(count))
-# print("Total: {}".format(total))
-# infile.close()
-# print()
+for eachline in infile:
+    count += 1
+    total += int(eachline)
 
 
-# #----------------------------------------------
+print("Numbers of numbers: {}".format(count))
+print("Total: {}".format(total))
+infile.close()
+print()
 
 
-# #Exercise 2: sum of digits
-# print("---Exercise 2: sum of digits---")
-# countchar = 0
-# sumchar = 0
 
-# number = input("Provide a number: > ")
-# for eachchar in number:
-#     countchar += 1
-#     sumchar += int(eachchar)
+#----------------------------------------------
+#Exercise 2: sum of digits
+print("---Exercise 2: sum of digits---")
+countchar = 0
+sumchar = 0
 
-# print("There are {} digits and the sum of the digits is {}.".format(countchar,sumchar))
-# print()
+number = input("Provide a number: > ")
+for eachchar in number:
+    countchar += 1
+    sumchar += int(eachchar)
 
-
-# #----------------------------------------------
-
-
-# #Optional : Sanitize a string
-# print("---Optional : Sanitize a string---")
-# string = input("Please input a string: > ")
-# stringAfter = string.strip().replace("~","").replace("!","").replace("-","")
-# print("Sanitized string: > {}".format(stringAfter))
-# print()
+print("There are {} digits and the sum of the digits is {}.".format(countchar,sumchar))
+print()
 
 
-# #----------------------------------------------
+#----------------------------------------------
+#Optional : Sanitize a string
+print("---Optional : Sanitize a string---")
+string = input("Please input a string: > ")
+stringAfter = string.strip().replace("~","").replace("!","").replace("-","")
+print("Sanitized string: > {}".format(stringAfter))
+print()
 
 
-# #Optional: Volume of a cone
-# print("---Optional: Volume of a cone---")
-# import math
-# radius = int(input("What is the radius of your cone (in cm)? > "))
-# height = int(input("What is the height of your cone (in cm)? > "))
-# volume = math.pi*math.pow(radius,2)*height/3
-# print("The volume is {:.2f} cm^2".format(volume))
-# print()
+
+#----------------------------------------------
+#Optional: Volume of a cone
+print("---Optional: Volume of a cone---")
+import math
+radius = int(input("What is the radius of your cone (in cm)? > "))
+height = int(input("What is the height of your cone (in cm)? > "))
+volume = math.pi*math.pow(radius,2)*height/3
+print("The volume is {:.2f} cm^2".format(volume))
+print()
 
 
-# #----------------------------------------------
-
-
+#----------------------------------------------
 #Optional: Binary to Decimal
 print("---Optional: Binary to Decimal---")
-# binaryNum = input("Please input a binary number > ")
-# countlen = len(binaryNum)
-# decimal = 0
-# listDigit = []
-# order = 0
+binaryNum = input("Please input a binary number > ")
+countlen = len(binaryNum)
+decimal = 0
+listDigit = []
+order = 0
 
-mylist=[1,2,3,4]
-mylist[0]=5
-mylist.append(10)
-a = (1, 2, 3, 4, 5)
-x = sum(mylist)
-x = 42
-y = format(x,'b')
+for eachdigit in binaryNum:
+    countlen -= 1
+    binaryDigit = binaryNum[countlen]
+    listDigit.append(int(binaryDigit))
+    decimal += listDigit[order]*2**(order)
+    order += 1
 
-
-print(y)
-
-# for eachdigit in binaryNum:
-#     countlen -= 1
-#     binaryDigit = binaryNum[countlen]
-    
-    # temp = sum(int(binaryDigit)*2,10)
-    # decimal += temp*2**2
-    # listDigit.append(int(binaryDigit))
-    # decimal += listDigit[order]*2**(order)
-    # order += 1
-
-    
-# print("The number in decimal is {}.".format(decimal))
+print("The number in decimal is {}.".format(decimal))
